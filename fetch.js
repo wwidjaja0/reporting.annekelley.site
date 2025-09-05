3bd7ff798dcbfc358520063981d9279da91f0d36
William Widjaja <contact@wwidjaja.com>
2025-09-05 13:12:00 -0700

    Make function async

--- fetch.js
+++ fetch.js
@@ -23,46 +23,48 @@ export async function getData(resource, colNames) {
   }
 }
 
-export function userAgentsToWordCloud() {
+export async function userAgentsToWordCloud() {
   try {
-    return console.log(getData("static", ["userAgent"]));
-      // .map(item => {
-      //   const ua = item.userAgent;
-      //   let parts = [];
+    const response = await getData("static", ["userAgent"]);
+    const data = response.json();
 
-      //   // --- Browser + version ---
-      //   if (/Chrome\/(\d+)/.test(ua) && !/Edg\//.test(ua)) {
-      //     const version = ua.match(/Chrome\/(\d+)/)[1];
-      //     parts.push(`Chrome_${version}`);
-      //   } else if (/Version\/(\d+).+Safari\//.test(ua) && !/Chrome\//.test(ua)) {
-      //     const version = ua.match(/Version\/(\d+)/)[1];
-      //     parts.push(`Safari_${version}`);
-      //   } else if (/Firefox\/(\d+)/.test(ua)) {
-      //     const version = ua.match(/Firefox\/(\d+)/)[1];
-      //     parts.push(`Firefox_${version}`);
-      //   } else if (/Edg\/(\d+)/.test(ua)) {
-      //     const version = ua.match(/Edg\/(\d+)/)[1];
-      //     parts.push(`Edge_${version}`);
-      //   } else {
-      //     parts.push("Other");
-      //   }
+    return data.map(item => {
+      const ua = item.userAgent;
+      let parts = [];
 
-      //   // --- OS ---
-      //   if (/Windows NT 10/.test(ua)) {
-      //     parts.push("Windows_10");
-      //   } else if (/Windows NT 11/.test(ua)) {
-      //     parts.push("Windows_11");
-      //   } else if (/Mac OS X 10_15/.test(ua)) {
-      //     parts.push("MacOS_10_15");
-      //   } else if (/Mac OS X 11/.test(ua)) {
-      //     parts.push("MacOS_11");
-      //   } else if (/Linux/.test(ua)) {
-      //     parts.push("Linux");
-      //   }
+      // --- Browser + version ---
+      if (/Chrome\/(\d+)/.test(ua) && !/Edg\//.test(ua)) {
+        const version = ua.match(/Chrome\/(\d+)/)[1];
+        parts.push(`Chrome_${version}`);
+      } else if (/Version\/(\d+).+Safari\//.test(ua) && !/Chrome\//.test(ua)) {
+        const version = ua.match(/Version\/(\d+)/)[1];
+        parts.push(`Safari_${version}`);
+      } else if (/Firefox\/(\d+)/.test(ua)) {
+        const version = ua.match(/Firefox\/(\d+)/)[1];
+        parts.push(`Firefox_${version}`);
+      } else if (/Edg\/(\d+)/.test(ua)) {
+        const version = ua.match(/Edg\/(\d+)/)[1];
+        parts.push(`Edge_${version}`);
+      } else {
+        parts.push("Other");
+      }
 
-      //   return parts.join(" ");
-      // })
-      // .join(" ");
+      // --- OS ---
+      if (/Windows NT 10/.test(ua)) {
+        parts.push("Windows_10");
+      } else if (/Windows NT 11/.test(ua)) {
+        parts.push("Windows_11");
+      } else if (/Mac OS X 10_15/.test(ua)) {
+        parts.push("MacOS_10_15");
+      } else if (/Mac OS X 11/.test(ua)) {
+        parts.push("MacOS_11");
+      } else if (/Linux/.test(ua)) {
+        parts.push("Linux");
+      }
+
+      return parts.join(" ");
+    })
+      .join(" ");
   } catch (err) {
     console.error("Error processing user agents:", err);
     return "";
