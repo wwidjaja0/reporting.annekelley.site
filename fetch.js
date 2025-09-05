8e82af39fee058d01386260a2c73761d1cb01016
William Widjaja <contact@wwidjaja.com>
2025-09-05 13:05:11 -0700

    test word cloud for browser agents

--- fetch.js
+++ fetch.js
@@ -22,3 +22,49 @@ export async function getData(resource, colNames) {
     console.error("Proxy fetch failed:", err);
   }
 }
+
+export function userAgentsToWordCloud() {
+  try {
+    return getData("static", ["userAgent"])
+      .map(item => {
+        const ua = item.userAgent;
+        let parts = [];
+
+        // --- Browser + version ---
+        if (/Chrome\/(\d+)/.test(ua) && !/Edg\//.test(ua)) {
+          const version = ua.match(/Chrome\/(\d+)/)[1];
+          parts.push(`Chrome_${version}`);
+        } else if (/Version\/(\d+).+Safari\//.test(ua) && !/Chrome\//.test(ua)) {
+          const version = ua.match(/Version\/(\d+)/)[1];
+          parts.push(`Safari_${version}`);
+        } else if (/Firefox\/(\d+)/.test(ua)) {
+          const version = ua.match(/Firefox\/(\d+)/)[1];
+          parts.push(`Firefox_${version}`);
+        } else if (/Edg\/(\d+)/.test(ua)) {
+          const version = ua.match(/Edg\/(\d+)/)[1];
+          parts.push(`Edge_${version}`);
+        } else {
+          parts.push("Other");
+        }
+
+        // --- OS ---
+        if (/Windows NT 10/.test(ua)) {
+          parts.push("Windows_10");
+        } else if (/Windows NT 11/.test(ua)) {
+          parts.push("Windows_11");
+        } else if (/Mac OS X 10_15/.test(ua)) {
+          parts.push("MacOS_10_15");
+        } else if (/Mac OS X 11/.test(ua)) {
+          parts.push("MacOS_11");
+        } else if (/Linux/.test(ua)) {
+          parts.push("Linux");
+        }
+
+        return parts.join(" ");
+      })
+      .join(" ");
+  } catch (err) {
+    console.error("Error processing user agents:", err);
+    return "";
+  }
+}
